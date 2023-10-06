a844ecc8de04ca7bdfce784e039859b25a2d4a6e
drklbri <drklbri@gmail.com>
2023-10-06 22:24:36 +0300

    1

--- main.py
+++ main.py
@@ -1,76 +1,58 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib
 
-matplotlib.use('TkAgg')
-# гармонический
-
-
-def generate_harmonic_signal(frequency, duration, sampling_rate):
+# Функция для генерации сигнала
+def generate_signal(frequency, duration, sampling_rate, signal_type="harmonic"):
     t = np.arange(0, duration, 1 / sampling_rate)
-    signal = np.sin(2 * np.pi * frequency * t)
+    if signal_type == "harmonic":
+        signal = np.sin(2 * np.pi * frequency * t)
+    elif signal_type == "square":
+        signal = np.sign(np.sin(2 * np.pi * frequency * t))
     return t, signal
 
-
-# цифровой
-def generate_square_wave(frequency, duration, sampling_rate):
-    t = np.arange(0, duration, 1 / sampling_rate)
-    signal = np.sign(np.sin(2 * np.pi * frequency * t))
-    return t, signal
-
-
+# Функция для вычисления спектра сигнала
 def compute_spectrum(signal, sampling_rate):
     spectrum = np.fft.fft(signal)
     frequencies = np.fft.fftfreq(len(signal), 1 / sampling_rate)
     return frequencies, spectrum
 
-
-#Параметры
+# Параметры
 frequencies = [1, 2, 4, 8]
 duration = 1.0
 sampling = 1000
 
-plt.figure(figsize=(14, 8 * len(frequencies)))
+# Создание сетки графиков
+fig, axs = plt.subplots(len(frequencies), 2, figsize=(12, 2 * len(frequencies)))
 
-for idx, frequency in enumerate(frequencies, 1):
-
-    t_harmonic, harmonic_signal = generate_harmonic_signal(frequency, duration, sampling)
-
-    t_square, square_signal = generate_square_wave(frequency, duration, sampling)
+for idx, frequency in enumerate(frequencies):
+    t_harmonic, harmonic_signal = generate_signal(frequency, duration, sampling, signal_type="harmonic")
+    t_square, square_signal = generate_signal(frequency, duration, sampling, signal_type="square")
 
     freq_harmonic, spectrum_harmonic = compute_spectrum(harmonic_signal, sampling)
     freq_square, spectrum_square = compute_spectrum(square_signal, sampling)
 
-    freq_harmonic_positive = freq_harmonic[freq_harmonic >= 0]
-    spectrum_harmonic_positive = np.abs(spectrum_harmonic[freq_harmonic >= 0])
-    freq_square_positive = freq_square[freq_square >= 0]
-    spectrum_square_positive = np.abs(spectrum_square[freq_square >= 0])
+    # Отрисовка гармонического сигнала и его спектра
+    axs[idx, 0].plot(t_harmonic, harmonic_signal, color='blue')
+    axs[idx, 0].set_title(f"Гармонический сигнал {frequency} Гц")
+    axs[idx, 0].set_xlabel('Время (сек)')
+    axs[idx, 0].set_ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 3)
-    plt.plot(t_harmonic, harmonic_signal, color='green')
-    plt.title(f"Гармонический сигнал {frequency} Гц")
-    plt.xlabel('Время (сек)')
-    plt.ylabel('Амплитуда')
+    axs[idx, 1].plot(freq_harmonic, np.abs(spectrum_harmonic), color='blue')
+    axs[idx, 1].set_title(f"Спектр гармонического сигнала {frequency} Гц")
+    axs[idx, 1].set_xlabel('Частота (Гц)')
+    axs[idx, 1].set_ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 2)
-    plt.plot(freq_harmonic_positive, spectrum_harmonic_positive, color='green')
-    plt.title(f"Спектр гармонического сигнала {frequency} Гц")
-    plt.xlabel('Частота (Гц)')
-    plt.ylabel('Амплитуда')
-    plt.xlim(0, 15)
+    # Отрисовка цифрового сигнала и его спектра
+    axs[idx + len(frequencies), 0].plot(t_square, square_signal, color='green')
+    axs[idx + len(frequencies), 0].set_title(f"Цифровой сигнал {frequency} Гц")
+    axs[idx + len(frequencies), 0].set_xlabel('Время (сек)')
+    axs[idx + len(frequencies), 0].set_ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 1)
-    plt.plot(t_square, square_signal, color='green')
-    plt.title(f"Цифровой сигнал {frequency} Гц")
-    plt.xlabel('Время (сек)')
-    plt.ylabel('Амплитуда')
+    axs[idx + len(frequencies), 1].plot(freq_square, np.abs(spectrum_square), color='green')
+    axs[idx + len(frequencies), 1].set_title(f"Спектр цифрового сигнала {frequency} Гц")
+    axs[idx + len(frequencies), 1].set_xlabel('Частота (Гц)')
+    axs[idx + len(frequencies), 1].set_ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4)
-    plt.plot(freq_square_positive, spectrum_square_positive, color='green')
-    plt.title(f"Спектр цифрового сигнала {frequency} Гц")
-    plt.xlabel('Частота (Гц)')
-    plt.ylabel('Амплитуда')
-    plt.xlim(0, 15)
-
-plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.tight_layout()
 plt.show()
