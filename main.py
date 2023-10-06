fc14c5f970ef70b2013ca3ef5f63d643cc3ed979
drklbri <drklbri@gmail.com>
2023-10-06 22:43:59 +0300

    1

--- main.py
+++ main.py
@@ -4,61 +4,61 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 
-def generate_signal(frequency, duration, sampling_rate, signal_type='harmonic'):
-    t = np.arange(0, duration, 1 / sampling_rate)
-    if signal_type == 'harmonic':
-        signal = np.sin(2 * np.pi * frequency * t)
-    elif signal_type == 'square':
-        signal = np.sign(np.sin(2 * np.pi * frequency * t))
+def generate_sig(frequency, duration, sample_rate, waveform_type='harmonic'):
+    time_values = np.arange(0, duration, 1 / sample_rate)
+    if waveform_type == 'harmonic':
+        waveform = np.sin(2 * np.pi * frequency * time_values)
+    elif waveform_type == 'square':
+        waveform = np.sign(np.sin(2 * np.pi * frequency * time_values))
     else:
         raise ValueError("Недопустимый тип сигнала")
-    return t, signal
+    return time_values, waveform
 
-def compute_spectrum(signal, sampling_rate):
-    spectrum = np.fft.fft(signal)
-    frequencies = np.fft.fftfreq(len(signal), 1 / sampling_rate)
+def compute_frequency_spectrum(waveform, sample_rate):
+    spectrum = np.fft.fft(waveform)
+    frequencies = np.fft.fftfreq(len(waveform), 1 / sample_rate)
     return frequencies, spectrum
 
-frequencies = [1, 2, 4, 8]
-duration = 1.0
-sampling = 1000
+freq_values = [1, 2, 4, 8]
+duration_time = 1.0
+sample_rate = 1000
 
-plt.figure(figsize=(14, 8 * len(frequencies)))
+plt.figure(figsize=(14, 8 * len(freq_values)))
 
-for idx, frequency in enumerate(frequencies, 1):
-    t_harmonic, harmonic_signal = generate_signal(frequency, duration, sampling, signal_type='harmonic')
-    t_square, square_signal = generate_signal(frequency, duration, sampling, signal_type='square')
+for idx, freq in enumerate(freq_values, 1):
+    time_harmonic, harmonic_waveform = generate_waveform(freq, duration_time, sample_rate, waveform_type='harmonic')
+    time_square, square_waveform = generate_waveform(freq, duration_time, sample_rate, waveform_type='square')
 
-    freq_harmonic, spectrum_harmonic = compute_spectrum(harmonic_signal, sampling)
-    freq_square, spectrum_square = compute_spectrum(square_signal, sampling)
+    freq_harmonic, spectrum_harmonic = compute_frequency_spectrum(harmonic_waveform, sample_rate)
+    freq_square, spectrum_square = compute_frequency_spectrum(square_waveform, sample_rate)
 
     freq_harmonic_positive = freq_harmonic[freq_harmonic >= 0]
     spectrum_harmonic_positive = np.abs(spectrum_harmonic[freq_harmonic >= 0])
     freq_square_positive = freq_square[freq_square >= 0]
     spectrum_square_positive = np.abs(spectrum_square[freq_square >= 0])
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 3)
-    plt.plot(t_harmonic, harmonic_signal, color='green')
-    plt.title(f"Сигнал {frequency} Гц")
+    plt.subplot(len(freq_values), 4, idx * 4 - 3)
+    plt.plot(time_harmonic, harmonic_waveform, color='green')
+    plt.title(f"Сигнал {freq} Гц")
     plt.xlabel('Время (сек)')
     plt.ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 2)
+    plt.subplot(len(freq_values), 4, idx * 4 - 2)
     plt.plot(freq_harmonic_positive, spectrum_harmonic_positive, color='green')
-    plt.title(f"Спектр сигнала {frequency} Гц")
+    plt.title(f"Спектр сигнала {freq} Гц")
     plt.xlabel('Частота (Гц)')
     plt.ylabel('Амплитуда')
     plt.xlim(0, 15)
 
-    plt.subplot(len(frequencies), 4, idx * 4 - 1)
-    plt.plot(t_square, square_signal, color='green')
-    plt.title(f"Сигнал {frequency} Гц")
+    plt.subplot(len(freq_values), 4, idx * 4 - 1)
+    plt.plot(time_square, square_waveform, color='green')
+    plt.title(f"Сигнал {freq} Гц")
     plt.xlabel('Время (сек)')
     plt.ylabel('Амплитуда')
 
-    plt.subplot(len(frequencies), 4, idx * 4)
+    plt.subplot(len(freq_values), 4, idx * 4)
     plt.plot(freq_square_positive, spectrum_square_positive, color='green')
-    plt.title(f"Спектр сигнала {frequency} Гц")
+    plt.title(f"Спектр сигнала {freq} Гц")
     plt.xlabel('Частота (Гц)')
     plt.ylabel('Амплитуда')
     plt.xlim(0, 15)
